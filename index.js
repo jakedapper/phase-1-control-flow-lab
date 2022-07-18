3dc6c7bdaf58d6805d913a5248fd2b222b324532
jake dapper <jakedapper@gmail.com>
2022-07-18 16:46:29 -0500

    commit tree 2efa6816d29b8032c6c1d5f38ee98b94888be87d

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(height){
   // Write your code here!
+  if (height <= 400) {
+    return "This one is on me!"
+  } else if (height > 400 && height < 2000) {
+    return "That will be twenty bucks."
+  } else if (height < 2500 && height > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (height > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const isNYC =(city === "NYC") ? "Ok, sounds good." : "No go."
+  return isNYC;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  // let tip = 'generous'
+  // let response;
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
